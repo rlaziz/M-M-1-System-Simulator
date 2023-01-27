a57cb52c3730d4784f2f5869c4a3d449501fa774
rlaziz <rlaziz@bu.edu>
2023-01-27 21:34:30 +0400

    Added Exponent class implementation

---
+++ src/Exponent.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public class Exponent {
+    static double exponent(double lambda) { // A function that generates a value within a Exponential Distribution with given parameters
+        Random rand = new Random();
+        return -Math.log(1 - rand.nextDouble()) / lambda;
+    }
+}
